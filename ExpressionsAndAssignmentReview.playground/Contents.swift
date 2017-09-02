1965e8ae2b5fb04ff1a12c9d0f7b14d300a12a87
Paul Gross <pgross@maryville.edu>
2017-09-02 12:07:08 -0500

    Initial commit

---
+++ ExpressionsAndAssignmentReview.playground/Contents.swift
@@ -0,0 +1,17 @@
+/* Overview
+ 
+ Your assignment is to create your own programming problem using variables, operators, and assignment. Think about the Operators playground you completed. This assignment is like you are producing a page for that assignment. You will work on your playground page in a playground cloned from GitHub. It should include *at least* the following:
+ 
+ - three arithmetic operators
+ - one compound operator
+ - three variables
+ - one type conversion
+ 
+ It's not very exciting to have a bunch of random numbers in code. When writing this problem you should be creating a story around it. This need not be tremendously complex, just a simple story that motivates the values being used and gives them a context.
+ 
+ For example 40 / 5 isn't interesting, but writing a tree planting app that given a species of tree and how much space you have will estimate how many trees you can plant is moreso. (e.g., I have 40 sq ft and want to plant walnut trees. They need 5 sq ft of growing space around the trunk, how many can I fit?).
+ 
+ You should write your story bits in comments (e.g., between /* and */, or lines starting with // ) around where the code should go.
+ 
+*/
+
